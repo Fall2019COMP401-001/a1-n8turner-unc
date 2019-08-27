8895e93dc470325e35eaf30ba82f3b60423397e4
n8turner <n8turner@n8turner-pc>
2019-08-27 19:05:23 -0400

    Finished and put a basic comment

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,27 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		String output="";
+		System.out.println("How many customers are there?");
+		int customers = scan.nextInt();
 		
+		for (int i = 0; i<customers; i++) {
+			System.out.println("What is the customer's first, last name, and the number of items they are purchasing?");
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int items = scan.nextInt();
+			double sum = 0.0;
+			System.out.println();
+			for (int j = 0; j < items; j++) {
+				System.out.println("How many of this item is the customer purchasing, what is the name of the item, and how much does one unit cost?");
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				double price = scan.nextDouble();
+				sum+= (double)quantity * price;
+			}
+			output = output + firstName.charAt(0) + ". " + lastName + " " + String.format("%.2f", sum) + "\n"; 
+		}
+		System.out.print(output);
 	}
+	
 }
