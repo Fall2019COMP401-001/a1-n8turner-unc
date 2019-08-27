db604c0b36964b63b86caaad058a96a34f57d131
n8turner <n8turner@n8turner-pc>
2019-08-27 19:13:11 -0400

    Put comments in, showing function

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
 		Scanner scan = new Scanner(System.in);
 		String output="";
 		int customers = scan.nextInt();
-		//iterates through customers
+		//iterates through customers, collecting data
 		for (int i = 0; i<customers; i++) {
 			String firstName = scan.next();
 			String lastName = scan.next();
 			int items = scan.nextInt();
 			double sum = 0.0;
-			for (int j = 0; j < items; j++) {
+			for (int j = 0; j < items; j++) {  //collects data for each item and sums
 				int quantity = scan.nextInt();
 				String item = scan.next();
 				double price = scan.nextDouble();
 				sum+= (double)quantity * price;
 			}
+			//constructs output
 			output = output + firstName.charAt(0) + ". " + lastName + ": " + String.format("%.2f", sum) + "\n"; 
 		}
 		System.out.print(output);
