abcf24afca5f4fee76b8187cef86057fc9b0e63b
n8turner <n8turner@n8turner-pc>
2019-08-28 12:45:59 -0400

    added comments

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,51 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		int count = scan.nextInt();
+		double[] prices = new double[count];
+		String[] itemNames = new String[count];
+		
+		for(int i = 0; i < count; i++) { // collects item data
+			itemNames[i] = scan.next();
+			prices[i] = scan.nextDouble(); 
+		}
+		
+		int customers = scan.nextInt();
+		int multiplier = 0;
+		String naming = "";
+		int numItems = 0;
+		int index = 0;
+		String[] firstNames = new String[customers];
+		String[] lastNames = new String[customers];
+		int[] itemAmounts = new int[count];
+		int[] numCustomers = new int[count];
+		for (int i = 0; i < customers; i++) { //collects inputs for each customer
+			double bill = 0.0;
+			firstNames[i] = scan.next();
+			lastNames[i] = scan.next();
+			numItems = scan.nextInt();
+			for(int j = 0; j < numItems; j++) {		//associates customer items through names
+				int numBought = scan.nextInt();
+				naming = scan.next();
+				for (int k = 0; k < itemNames.length; k++) {  //counts item amounts
+					if(naming.equals(itemNames[k])) {
+						numCustomers[k]++;
+						for (int l = 0; l < numBought; l++) {
+							itemAmounts[k]++;
+						}
+					}
+				}
+			}
+		}
+		
+		for(int i = 0; i < count; i++) {  //prints output
+			if(itemAmounts[i]==0) {
+				System.out.println("No customers bought " + itemNames[i]);
+			}
+			else {
+				System.out.println(numCustomers[i] + " customers bought " + itemAmounts[i] + " " + itemNames[i]);
+			}
+		}
 		
 	}
 }
